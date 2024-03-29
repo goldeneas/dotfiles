26699eef45ac699a930249f16310b3c0e9dca62e
Nicola <nicola.picarella6@gmail.com>
2024-03-30 00:11:06 +0100

    new version

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -3,7 +3,7 @@ return {
 	event = "VeryLazy",
 	init = function()
 		vim.o.timeout = true
-		vim.o.timeoutlen = 300
+		vim.o.timeoutlen = 500
 	end,
 	config = function()
 		local whichkey = require("which-key")
@@ -12,8 +12,13 @@ return {
 		whichkey.register({
 			f = {
 				name = "File",
-				f = { "<cmd>Telescope find_files<cr>", "Find File" },
+				f = { "<cmd>Telescope frecency<cr>", "Find File" },
 				r = { "<cmd>Telescope oldfiles<cr>", "Open Recent Files" },
+				d = { "<cmd>Telescope file_browser<cr>", "Open File Browser" },
+			},
+			d = {
+				name = "Directory",
+				d = { "<cmd>Dirbuf<cr>", "Open Dirbuf" },
 			},
 			w = {
 				name = "Window",
@@ -30,16 +35,15 @@ return {
 				t = { vim.lsp.buf.type_definition, "Check Type Definition" },
 				u = { "<cmd>Telescope lsp_references show_line=false<cr>", "Check Usages" },
 			},
-			d = {
-				name = "Diagnostics",
-				d = { vim.diagnostic.open_float, "Check Local Error" },
-				t = { "<cmd>TroubleToggle<cr>", "Toggle Errors Tab" },
-				c = { "<cmd>TroubleClose<cr>", "Close Errors Tab" },
-			},
 			s = {
 				name = "Switch Tabs",
 				s = { function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, "Tab List" },
 				a = { function() harpoon:list():append() end, "Append Tab" },
+			},
+			x =  {
+				x = { "<cmd>TroubleToggle<cr>", "Toggle Errors Tab" },
+				l = { vim.diagnostic.open_float, "Check Local Error" },
+				q = { "<cmd>Trouble quickfix<cr>", "Toggle Quickfix Tab" },
 			},
 			z = { "<cmd>HopWord<cr>", "Hop Word" },
 		}, { prefix = "<leader>" })
