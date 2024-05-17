2fdbbe43df3ebcf697af6d9c7deea2caa1fbdb06
Nicola <nicola.picarella6@gmail.com>
2024-05-17 23:08:07 +0200

    added illuminated -> removed cursorline

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,2 +1,7 @@
-vim.cmd([[hi! link NormalFloat Normal]])
-vim.cmd([[hi! link FloatBorder Normal]])
+vim.cmd("hi! link NormalFloat Normal")
+vim.cmd("hi! link FloatBorder Normal")
+
+vim.cmd("hi! illuminatedWord gui=underline")
+vim.cmd("hi! IlluminatedWordText gui=underline")
+vim.cmd("hi! IlluminatedWordRead gui=underline")
+vim.cmd("hi! IlluminatedWordWrite gui=underline")
