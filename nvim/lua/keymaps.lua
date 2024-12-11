ef36ea4c3c0a62c4f4e53f002a00c826e23fe3c9
Nicola <nicola.picarella6@gmail.com>
2024-12-11 22:25:46 +0100

    feat: update
    removed inc-rename, added keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -9,12 +9,6 @@ local map = function(mode, keys, func, desc)
 	vim.keymap.set(mode, keys, func, options)
 end
 
--- KEYMAPS
-
-vim.keymap.set("n", "<leader>rn", function()
-  return ":IncRename " .. vim.fn.expand("<cword>")
-end, { desc = "[R]e[n]ame", expr = true })
-
 -- Window
 map("n", "<leader>ws", "<cmd>split<cr>", "[S]plit [W]indow Horizontally")
 map("n", "<leader>wS", "<cmd>vsplit<cr>", "[S]plit [W]indow Vertically")
@@ -26,18 +20,20 @@ map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", "[F]ind [S]tring")
 map("n", "-", "<cmd>Oil<cr>", "[F]ind [D]irectory")
 map("n", "<F3>", "<cmd>UndotreeToggle<cr>", "Toggle Undotree")
 
-map("n", "<esc>", "<cmd>nohlsearch<cr>", "Remove Search Highlights")
-
 -- LSP/Code
--- TODO: Make this only work after attaching
-map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", "Show Errors Tab")
-map("n", "<leader>xl", vim.diagnostic.open_float, "Show [L]ocal Error")
-map("n", "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ctions")
-map("n", "K", vim.lsp.buf.hover, "Hover Symbol")
-map("n", "<C-k>", vim.lsp.buf.type_definition, "Check Type Definition")
-map("n", "ò", vim.diagnostic.goto_next, "Goto Next Diagnostic")
-map("n", "à", vim.diagnostic.goto_prev, "Goto Prev Diagnostic")
-map("n", "<leader>xr", vim.lsp.buf.references, "Show [R]eferences")
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(_)
+        map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", "Show Errors Tab")
+        map("n", "<leader>xl", vim.diagnostic.open_float, "Show [L]ocal Error")
+        map("n", "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ctions")
+        map("n", "K", vim.lsp.buf.hover, "Hover Symbol")
+        map("n", "<C-k>", vim.lsp.buf.type_definition, "Check Type Definition")
+        map("n", "ò", vim.diagnostic.goto_next, "Goto Next Diagnostic")
+        map("n", "à", vim.diagnostic.goto_prev, "Goto Prev Diagnostic")
+        map("n", "<leader>xr", vim.lsp.buf.references, "Show [R]eferences")
+        map("n", "<leader>r", vim.lsp.buf.rename, "[R]ename")
+    end,
+})
 
 -- Goto
 map("n", "f", "<Plug>(leap)", "Leap")
@@ -46,6 +42,8 @@ map("n", "gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 map("n", "gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
 map("n", "gr", "<cmd>Telescope lsp_references show_line=false<cr>", "[G]oto [R]eferences")
+map({"n", "x"}, "<C-h>", "^", "Goto Line Start")
+map({"n", "x"}, "<C-l>", "g_", "Goto Line End")
 
 -- DAP
 map("n", "<F5>", "<cmd>DapContinue<cr>", "Dap Continue")
@@ -53,11 +51,10 @@ map("n", "<F10>", "<cmd>DapStepOver<cr>", "Dap StepOver")
 map("n", "<F11>", "<cmd>DapStepInto<cr>", "Dap StepInto")
 map("n", "<leader>b", "<cmd>DapToggleBreakpoint<cr>", "Dap Toggle Breakpoint")
 
--- Window Movement
-map("n", "<C-h>", "<C-w>h", "Goto Left Window")
-map("n", "<C-k>", "<C-w>k", "Goto Up Window")
-map("n", "<C-j>", "<C-w>j", "Goto Down Window")
-map("n", "<C-l>", "<C-w>l", "Goto Right Window")
+-- Quickfix
+map("n", "<C-m>", f.toggle_qf, "Toggle Quickfix List")
+map("n", "<C-j>", "<cmd>cnext<cr>", "Next Quickfix Entry")
+map("n", "<C-k>", "<cmd>cprev<cr>", "Prev Quickfix Entry")
 
 -- Keep current search item at center of window
 map("n", "n", "nzzzv", "Search Next")
@@ -65,3 +62,6 @@ map("n", "N", "Nzzzv", "Search Previous")
 
 -- Disables replacing current clipboard after pasting
 map("x", "p", "\"_dP", "Just Paste")
+
+-- Remove highlighting from search
+map("n", "<esc>", "<cmd>nohlsearch<cr>", "Remove Search Highlights")
--- nvim/lua/plugins/inc-rename.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"smjonas/inc-rename.nvim",
-	config = function()
-		local increname = require("inc_rename")
-		increname.setup({
-			input_buffer_type = "dressing",
-		})
-	end
-}
