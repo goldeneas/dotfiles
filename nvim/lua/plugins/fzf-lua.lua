11bc4fe0caa10968a83f24ced7a165a61616810e
Nicola <nicola.picarella6@gmail.com>
2024-12-19 21:42:26 +0100

    feat: added customization for fzf-lua

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,13 +1,48 @@
 return {
   "ibhagwan/fzf-lua",
-  -- optional for icon support
   dependencies = {
       "nvim-tree/nvim-web-devicons",
       { "junegunn/fzf", build = "./install --bin" }
   },
   config = function()
-    -- calling `setup` is optional for customization
-    require("fzf-lua").setup({})
-  end
+    require("fzf-lua").setup({
+        defaults = {
+            formatter = "path.filename_first",
+            previewer = false,
+            prompt = "> ",
+            header = false,
 
+            winopts = {
+                treesitter = true,
+                backdrop = 100,
+                width = 0.5,
+                height = 0.5,
+            },
+
+            fzf_colors = {
+                ["gutter"] = "-1",
+                ["marker"] = "Red",
+                ["pointer"] = "Red",
+            },
+
+            fzf_opts = {
+                ["--layout"] = "default",
+                ["--marker"] = "+",
+                ["--prompt"] = "> ",
+            },
+        },
+
+        profiles = {
+            previewer = true,
+
+            winopts = {
+                height = 0.8,
+                width = 0.8,
+                preview = {
+                    scrollbar = false,
+                },
+            }
+        }
+    })
+  end
 }
