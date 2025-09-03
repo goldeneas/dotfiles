3e3496f9ded01bdfda75d5a5c6442fa16ae218bc
nicola <nicola.picarella6@gmail.com>
2025-09-03 13:02:32 +0200

    Updated JDTLS settings

--- nvim/lua/lsp/config/jdtls.lua
+++ nvim/lua/lsp/config/jdtls.lua
@@ -56,6 +56,10 @@ local config = {
                 "switchExpression",
             },
         },
+
+        codeGeneration = {
+            insertionLocation = "lastMember",
+        },
     }
   },
 
