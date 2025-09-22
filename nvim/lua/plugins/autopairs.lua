5caa6b0863977a82617ac0dd38bc952092c53921
nicola <nicola.picarella6@gmail.com>
2025-09-22 22:40:25 +0200

    chore: update

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,12 +1,9 @@
 return {
 	"windwp/nvim-autopairs",
     event = "VeryLazy",
-	config = function()
-        local autopairs = require("nvim-autopairs")
-        autopairs.setup({
-            check_ts = true,
-            -- enable_moveright = false,
-            enable_check_bracket_line = false
-        })
-    end
+    opts = {
+        check_ts = true,
+        -- enable_moveright = false,
+        enable_check_bracket_line = false
+    }
 }
