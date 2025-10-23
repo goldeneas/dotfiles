efed9aac212eee95014866ef76afa2896777ce7e
Nicola <nicola.picarella6@gmail.com>
2025-10-23 10:41:48 +0200

    chore: better directory structuring

--- update.sh
+++ update.sh
@@ -4,13 +4,14 @@ if [[ $# < 1 ]]; then
 fi
 
 target=$1
+local_config_path=./home/.config
 
 if [[ $target == "nvim" ]]; then
     echo "Removing nvim folder..."
-    rm -rf nvim
+    rm -rf $local_config_path/nvim
 
     echo "Updating nvim..."
-    cp -rf $HOME/.config/nvim .
+    cp -rf $HOME/.config/nvim $local_config_path
 
     echo "Done!"
     exit 1
@@ -18,10 +19,10 @@ fi
 
 if [[ $target == "zsh" ]]; then
     echo "Removing zsh folder..."
-    rm .zshrc
+    rm $local_config_path/.zshrc
 
     echo "Updating zsh..."
-    cp $HOME/.zshrc .
+    cp $HOME/.zshrc $local_config_path
 
     echo "Done!"
     exit 1
@@ -29,10 +30,10 @@ fi
 
 if [[ $target == "tmux" ]]; then
     echo "Removing tmux folder..."
-    rm .tmux.conf
+    rm $local_config_path/.tmux.conf
 
     echo "Updating tmux..."
-    cp $HOME/.tmux.conf .
+    cp $HOME/.tmux.conf $local_config_path
 
     echo "Done!"
     exit 1
