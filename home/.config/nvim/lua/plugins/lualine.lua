1b2193a7c1b115ae87672fdc6833b6ee687e9320
nicola <nicola.picarella6@gmail.com>
2025-10-30 21:50:51 +0100

    updated lualine

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -1,22 +1,55 @@
-local function cwd_relative_path()
+local cwd_relative_path = function()
   local full_path = vim.fn.expand('%:p:h') -- directory of current file
   local rel_path = vim.fn.fnamemodify(full_path, ':.' ) -- relative to cwd
   return rel_path ~= '' and rel_path or '.'
 end
 
+local filename_component = function()
+    return {
+        "filename",
+        padding = { left = 1, right = 0 },
+    }
+end
+
+local lsp_status_component = function()
+    return {
+        "lsp_status",
+        icon = "",
+        symbols = {
+            done = "",
+            spinner = { "!" },
+            separator = " | "
+        },
+        show_name = true,
+        padding = { left = 0, right = 1 },
+        fmt = function(str)
+            if str == "" then return "" end
+            return "[" .. str .. "]"
+        end
+    }
+end
+
+local branch_component = function()
+    return {
+        "branch",
+        icon = "",
+        padding = { left = 0, right = 2 },
+    }
+end
+
 return {
     'nvim-lualine/lualine.nvim',
-    -- dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-        local lualine = require("lualine")
-        lualine.setup({
-			theme = "gruvbox",
-			sections = {
-                lualine_b = { "branch", "diagnostics" },
-				lualine_x = {  cwd_relative_path },
-                lualine_y = { "" },
-                lualine_z = { "" },
-			}
-		})
-	end
+    opts = {
+        options ={
+            component_separators = "",
+            section_separators = "",
+        },
+        sections = {
+            lualine_b = { branch_component(), "diagnostics" },
+            lualine_c = { filename_component(), lsp_status_component()},
+            lualine_x = {  cwd_relative_path },
+            lualine_y = { "" },
+            lualine_z = { "" },
+        }
+    }
 }
