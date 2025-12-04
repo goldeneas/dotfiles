62d2c3bc879b2f2a815f5f88091ab839142bbd88
Nicola <nicola.picarella6@gmail.com>
2025-12-04 21:36:39 +0100

    removed autopairs, added surround

--- home/.config/nvim/lua/plugins/autopairs.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"windwp/nvim-autopairs",
-    event = "VeryLazy",
-    opts = {
-        check_ts = true,
-        -- enable_moveright = false,
-        enable_check_bracket_line = false
-    }
-}
