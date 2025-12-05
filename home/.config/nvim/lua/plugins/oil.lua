90c5a8b442e992a92a8c94cdaad1a9a8dacbcaaf
nicola <nicola.picarella6@gmail.com>
2025-12-05 22:34:19 +0100

    removed vim-rooter, readded autopairs, added gitsigns

--- home/.config/nvim/lua/plugins/oil.lua
+++ home/.config/nvim/lua/plugins/oil.lua
@@ -1,4 +1,14 @@
 return {
 	"stevearc/oil.nvim",
-    opts = {},
+    opts = {
+        view_options = {
+            show_hidden = true,
+        },
+
+        float = {
+            padding = 2,
+            max_width = 90,
+            max_height = 0,
+        },
+    },
 }
