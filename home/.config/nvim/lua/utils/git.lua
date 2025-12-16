753a8f20b03e64530c3679ddea719b9c62922ac0
Nicola <nicola.picarella6@gmail.com>
2025-12-16 15:06:47 +0100

    better lazygit+toggleterm integration

--- home/.config/nvim/lua/utils/git.lua
+++ home/.config/nvim/lua/utils/git.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 local Terminal = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local lazygit = Terminal:new({
+    cmd = "lazygit",
+    hidden = true,
+    count = 99,
+})
 
 M.toggle_lazygit = function()
     lazygit:toggle()
