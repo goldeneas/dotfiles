baf4f9f1485ebdb2f55f92c4daab0bd96ce13f6a
nicola <nicola.picarella6@gmail.com>
2025-12-18 17:59:30 +0100

    added neoscroll

--- home/.config/nvim/lua/utils/ui.lua
+++ home/.config/nvim/lua/utils/ui.lua
@@ -1,6 +1,15 @@
 local oil = require("oil")
+local neoscroll = require("neoscroll")
 
 local M = {}
+
+M.scroll_up = function()
+    neoscroll.scroll(-10, { move_cursor = true, duration = 30 })
+end
+
+M.scroll_down = function()
+    neoscroll.scroll(10, { move_cursor = true, duration = 30 })
+end
 
 M.win_is_qf_or_loc = function(win_id)
     local win_info = vim.fn.getwininfo(win_id)[1]
