16a6ba9ea40ba09b9a4f5f99c5fca5b457912caf
nicola <nicola.picarella6@gmail.com>
2025-12-28 14:54:46 +0100

    feat: added term close keymap toggleterm

--- home/.config/nvim/lua/plugins/toggleterm.lua
+++ home/.config/nvim/lua/plugins/toggleterm.lua
@@ -5,5 +5,9 @@ return {
         direction = "float",
         close_on_exit = true,
         persist_mode = false,
+
+        on_open = function(term)
+            vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<esc>", "<cmd>close<cr>", {})
+        end,
     }
 }
