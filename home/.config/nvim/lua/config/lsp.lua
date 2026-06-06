8d01202e95afd223c6f7b24e835ed0334e6f461b
Nicola <nicola.picarella6@gmail.com>
2026-06-07 00:03:58 +0200

    feat: disable snippet support

--- home/.config/nvim/lua/config/lsp.lua
+++ home/.config/nvim/lua/config/lsp.lua
@@ -5,12 +5,17 @@ vim.filetype.add({
     }
 })
 
--- pass blink.cmp capabilities
+-- pass capabilities
 -- NOTE: this might not be needed soon, as blink will be adding this themselves
-local base_capabilities = vim.lsp.protocol.make_client_capabilities()
-local full_capabilities = require('blink.cmp').get_lsp_capabilities(base_capabilities)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+capabilities = vim.tbl_extend("force", capabilities, {
+    -- disable snippet support
+    textDocument = { completion = { completionItem = { snippetSupport = false } } },
+})
+
 vim.lsp.config("*", {
-    capabilities = full_capabilities,
+    capabilities = capabilities,
 })
 
 -- load LSPs that have been manually integrated with neovim.
--- home/.config/nvim/lua/plugins/blink-cmp.lua
+++ home/.config/nvim/lua/plugins/blink-cmp.lua
@@ -100,6 +100,11 @@ return {
                     },
                 },
             },
+            transform_items = function(_, items)
+                return vim.tbl_filter(function(item)
+                    return item.kind ~= require('blink.cmp.types').CompletionItemKind.Snippet
+                end, items)
+            end
         }
     }
 }
